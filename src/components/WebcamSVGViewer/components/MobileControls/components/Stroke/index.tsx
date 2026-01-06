691f7dfae8e151f26606fece2d4ad165e6c36dcd
dotkalm <joelmholmberg@gmail.com>
2026-01-06 17:57:55 -0500

    toggle styles

--- src/components/WebcamSVGViewer/components/MobileControls/components/Stroke/index.tsx
+++ src/components/WebcamSVGViewer/components/MobileControls/components/Stroke/index.tsx
@@ -1,7 +1,10 @@
 import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
+import { alpha, styled } from '@mui/material/styles';
 import type { TStrokeProps } from '@/types';
 import { 
     sliderContainer,
@@ -11,6 +14,9 @@ import {
 
 export default function Stroke({ configs, strokeColor }: TStrokeProps) {
     const theme = useTheme();
+    const strokeOpacity = configs['Stroke Opacity'].value;
+    const strokeEnabled = strokeOpacity > 0;
+    const label = !strokeEnabled ? 'Show' : 'Hide';
     return (
         <Box
             sx={{
@@ -23,7 +29,7 @@ export default function Stroke({ configs, strokeColor }: TStrokeProps) {
                 gap: 1,
             }}
         >
-            {Object.keys(configs).map((label: string, index: number) => {
+            {Object.keys(configs).map((label: string) => {
                 const {
                     changeHandler,
                     max,
@@ -78,37 +84,91 @@ export default function Stroke({ configs, strokeColor }: TStrokeProps) {
                     </Box>
                 );
             })}
-            <Box 
+            <Box
                 sx={{
-                    alignItems: 'flex-end',
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    mb: 1,
-                    px: '4dvw',
-                    width: '100%',
+                display: 'flex',              
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'row',
                 }}
             >
-            <Box 
-                sx={{
-                    width: '100%',
-                    mb: 1,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                    gap: 2,
-                }}
-            >
-                <Typography variant="caption" color="text.secondary">
-                    Stroke Color
-                </Typography>
-                <input
-                    type="color"
-                    value={strokeColor.value}
-                    onChange={strokeColor.changeHandler}
-                    style={{ cursor: 'pointer', height: '24px', width: '48px', border: 'none', borderRadius: '4px' }}
-                />
-            </Box>
+                <Box>
+                    <FormControlLabel
+                        control={
+                            <StyledSwitch
+                                checked={strokeEnabled}
+                                slotProps={{ input: { 'aria-label': label } }}
+                                onChange={(e) => {
+                                    if (strokeEnabled) {
+                                        configs['Stroke Opacity'].changeHandler(e as unknown as Event, 0);
+                                    } else {
+                                        configs['Stroke Opacity'].changeHandler(e as unknown as Event, 100);
+                                    }
+                                }}
+                            />
+                        }
+                        label={label}                    
+                    />
+                </Box>
+                <Box
+                    sx={{
+                        alignItems: 'flex-end',
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        mb: 1,
+                        px: '4dvw',
+                        width: '100%',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: '100%',
+                            mb: 1,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'flex-end',
+                            gap: 2,
+                        }}
+                    >
+                        <Typography variant="caption" color="text.secondary">
+                            Stroke Color
+                        </Typography>
+                        <input
+                            type="color"
+                            value={strokeColor.value}
+                            onChange={strokeColor.changeHandler}
+                            style={{ cursor: 'pointer', height: '24px', width: '48px', border: 'none', borderRadius: '4px' }}
+                        />
+                    </Box>
+                </Box>
             </Box>
         </Box>
     );
-}
+};
+
+const StyledSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase': {
+    // Unchecked state (default)
+    '& .MuiSwitch-thumb': {
+      backgroundColor: 'green !important',
+      border: '2px solid orange !important',
+    },
+    '& + .MuiSwitch-track': {
+      backgroundColor: 'gold !important',
+      border: '2px solid purple !important',
+      opacity: '1 !important',
+    },
+    // Checked state
+    '&.Mui-checked': {
+      '& .MuiSwitch-thumb': {
+        backgroundColor: 'blue !important',
+        border: '2px solid red !important',
+      },
+      '& + .MuiSwitch-track': {
+        backgroundColor: 'silver !important',
+        border: '2px solid black !important',
+        opacity: '1 !important',
+      },
+    },
+  },
+}));
